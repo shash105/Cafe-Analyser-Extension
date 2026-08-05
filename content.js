81b6ec1c69cb13b025308d6325fb22ac7425b84e
shash105 <shashmiprahan@gmail.com>
2026-08-05 13:18:36 +0100

    Added the feature of automatic clicking of 'more user reviews' button

--- content.js
+++ content.js
@@ -1,10 +1,37 @@
-const interval = setInterval(()=>{
-    const reviews = getReviews();
+function clickMoreReviews() {
 
-    if (reviews.length === 0) return;
-    
-    clearInterval(interval);
+    const elements = [...document.querySelectorAll("*")];
 
-    const summary = analyseReviews(reviews);
-    createPanel(summary);
+    const button = elements.find(el =>
+        el.innerText?.trim() === "More user reviews"
+    );
+
+    if (button) {
+        console.log("Found More user reviews button");
+        button.click();
+        return true;
+    }
+
+    return false;
+
+}
+
+const interval = setInterval(() => {
+
+    if (clickMoreReviews()) {
+
+        clearInterval(interval);
+
+        setTimeout(() => {
+
+            const reviews = getReviews();
+
+            const summary = analyseReviews(reviews);
+
+            createPanel(summary);
+
+        }, 2000);
+
+    }
+
 }, 1000);
